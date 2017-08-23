cf6c600bccde77d5ff28916b633227efa295e6b4
otukutun <orehaorz@gmail.com>
2017-08-23 22:37:39 +0900

    split with br.

--- lib/himekaminize/task_list.rb
+++ lib/himekaminize/task_list.rb
@@ -3,12 +3,19 @@ module Himekaminize
     # @param markdown [String]
     def initialize(markdown)
       @markdown = markdown
+      to_lines
     end
 
     # @todo
     # @param markdown [String]
     def to_a
-      []
+      @lines
+    end
+
+    private
+
+    def to_lines
+      @lines = @markdown.lines
     end
   end
 end
